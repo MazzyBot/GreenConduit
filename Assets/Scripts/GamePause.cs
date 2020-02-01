1021814a5ac32f6572c0314364d596776bb48139
Matt B <Matt.Barrett@live.com.au>
2020-02-01 17:51:56 +1100

    Main menu and model importing

---
+++ Assets/Scripts/GamePause.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePause : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pausePanel.activeInHierarchy)
+            {
+                pause();
+            }
+            else
+            if (pausePanel.activeInHierarchy)
+            {
+                unPause();
+            }
+        }
+    }
+
+    public void pause()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+
+    public void unPause()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+}
